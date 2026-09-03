53740e2a38ebe9e029b3b37cacc161c02fc7d7a7
Himanshu Khadka <hkhadka@Himanshus-MacBook-Air.local>
2026-09-03 17:18:52 -0600

    main activity updated

--- PetShop/app/src/main/java/com/himanshukhadka/petshop/MainActivity.kt
+++ PetShop/app/src/main/java/com/himanshukhadka/petshop/MainActivity.kt
@@ -2,16 +2,7 @@ package com.himanshukhadka.petshop
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.himanshukhadka.petshop.ui.theme.PetShopTheme
+
 
 val cat = Cat("Coco", 6)
 val dog = Dog("Mochi", 6)
@@ -35,32 +26,7 @@ fun pets(){
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PetShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PetShopTheme {
-        Greeting("Android")
-    }
-}
